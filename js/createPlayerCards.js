21f2ca0e0c84982e1a056a152d00d414d133885c
Brandon <brandonesser435@gmail.com>
2024-09-14 02:08:00 -0700

    Update name sort, remove gamertag, clean HTML

--- js/createPlayerCards.js
+++ js/createPlayerCards.js
@@ -4,27 +4,32 @@
 // global variable thingy fr
 let players = [];
 const membersContainer = document.querySelector('.members');
+let displayMode = 'name'; // Default to displaying name
+
+// Name or gamertag display toggle
+document.getElementById('toggle-display').addEventListener('change', (event) => {
+    displayMode = event.target.value;
+    renderPlayerCards(players);
+});
 
 // function that makea da card 
 function renderPlayerCards(players) {
-
-    // eraser
     membersContainer.innerHTML = '';
 
     players.forEach(player => {
         const memberDiv = document.createElement('div');
         memberDiv.classList.add('member');
-
         memberDiv.style.backgroundImage = `url(${player.backgroundImage})`;
+
+        const displayName = displayMode === 'name' ? player.name : player.gamertag;
 
         const playerCard = `
             <p><img class="img" src="https://starlightskins.lunareclipse.studio/render/ultimate/${player.gamertag}/full" height="150" alt="${player.name} body"></p>
             <p><img src="${player.org}" class="icon"></p>
             <div class="memberinfo">
-                <p class="name"><img class="img" src="https://mc-heads.net/avatar/${player.gamertag}" height="30" width="30"> ${player.name}</p>
+                <p class="name"><img class="img" src="https://mc-heads.net/avatar/${player.gamertag}" height="30" width="30"> ${displayName}</p>
                 <p><b>Online:</b> ${player.online}</p>
                 <p><b>Discord:</b> ${player.discord}</p>
-                <p><b>Gamertag:</b> ${player.gamertag}</p>
                 <p><b>Religion:</b> ${player.religion}</p>
                 <p><b>Nation(s):</b> ${player.nations.join(', ')}</p>
             </div>
@@ -32,25 +37,26 @@ function renderPlayerCards(players) {
 
         memberDiv.innerHTML = playerCard;
         memberDiv.id = player.name;
-        document.querySelector('.members').appendChild(memberDiv);
+        membersContainer.appendChild(memberDiv);
     });
 }
+
+// Function to sort players by nations
 function sortByNations(players) {
     return [...players].sort((a, b) => {
         const nationA = a.nations[0] || '';
         const nationB = b.nations[0] || '';
         const nationComparison = nationA.localeCompare(nationB);
 
-        //if nations are the same, compare by names
+        // if nations are the same, compare by names
         if (nationComparison === 0) {
             return a.name.localeCompare(b.name);
         }
-
         return nationComparison;
     });
 }
 
-// real code starts
+// Load player data from JSON
 fetch('../data/players.json')
     .then(response => response.json())
     .then(data => {
@@ -62,30 +68,30 @@ fetch('../data/players.json')
     });
 
 
-// GRAHHH SORT
-
-//name
+// TODO: Keep sort option on name display update
+// Name sort, toggles between name and gamertag
 document.getElementById('sort-name').addEventListener('click', () => {
-    const sortedPlayers = [...players].sort((a, b) => a.name.localeCompare(b.name));
+    const sortedPlayers = [...players].sort((a, b) => {
+        if (displayMode === 'name') {
+            return a.name.localeCompare(b.name);
+        } else {
+            return a.gamertag.localeCompare(b.gamertag);
+        }
+    });
     renderPlayerCards(sortedPlayers);
 });
-//gamertag
-document.getElementById('sort-gamertag').addEventListener('click', () => {
-    const sortedPlayers = [...players].sort((a, b) => a.gamertag.localeCompare(b.gamertag));
-    renderPlayerCards(sortedPlayers);
-});
-//discord
+
+// Discord sort
 document.getElementById('sort-discord').addEventListener('click', () => {
     const sortedPlayers = [...players].sort((a, b) => a.discord.localeCompare(b.discord));
     renderPlayerCards(sortedPlayers);
 });
-//nations
+
+// Nation sort
 document.getElementById('sort-nation').addEventListener('click', () => {
     const sortedPlayers = sortByNations(players);
     renderPlayerCards(sortedPlayers);
-});
-
-/*is this the true power of sex mode?*/
+})
 
 
 /*
@@ -94,7 +100,6 @@ This will not work locally because of CORS. Start a Chrome from PowerShell with 
 Start-Process "chrome.exe" -ArgumentList "--user-data-dir=`"C:\Chrome dev session`" --disable-web-security" 
 
 TODO:
-Move background image to players.json so this file can create it, and not have it be hard coded
 Cache player heads locally as calling an API every time is slow, and may be rate limited
 Don't use HTML in the JS, but create and set the elements directly with JS.
 */
